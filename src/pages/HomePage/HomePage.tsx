faf1f77f4acf209f5aba56e5535f5c05bb1d478b
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-05-30 00:03:15 +0200

    12-add-get-expense-option-to-api
    #12 added get expenses method, fixed saving cookies, fixed some tanstack setting.

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,30 @@
 import { Link } from 'react-router-dom';
 import { RoutesEnum } from 'src/shared/constants/routesEnum.ts';
+import { useQuery } from '@tanstack/react-query';
+import { getExpenses } from 'src/shared/api/api.ts';
 
 const HomePage = () => {
+  const { data } = useQuery({ queryFn: getExpenses, queryKey: ['expenses'] });
   return (
     <div>
       <h1>Home Page</h1>
       <Link to={RoutesEnum.Profile}>Go to Profile</Link>
+      <h2>Expenses</h2>
+      <ul>
+        {data?.map((expense) => (
+          <li key={expense._id}>
+            created:{expense.createdAt.toLocaleString()}
+            <br />
+            ammount:{expense.amount}
+            <br />
+            comments: {expense.comments}
+            <br />
+            categoryId:{expense.categoryId}
+            <br />
+            paymentSourceId:{expense.paymentSourceId}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const apiInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'https://shoplist-be.vercel.app/api/',
   timeout: 5000,
+  withCredentials: true,
 });
 
 export const login = async ({ email, password }: TLoginRequest): Promise<TLoginResponse> => {
@@ -19,4 +20,20 @@ type TLoginResponse = {
   isVerified: boolean;
   accessToken: string;
   refreshToken: string;
+};
+
+export const getExpenses = async (): Promise<TGetExpensesResponse> => {
+  const response = await apiInstance.get<TGetExpensesResponse>('expenses');
+  return response.data;
+};
+type TGetExpensesResponse = TExpense[];
+type TExpense = {
+  _id: string;
+  amount: number;
+  userId: string;
+  categoryId: string;
+  paymentSourceId: string;
+  comments?: string;
+  createdAt: Date;
+  updatedAt: Date;
 };
