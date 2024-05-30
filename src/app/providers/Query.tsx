845e4300f529d6d07822c596605999a274b0f98d
Aleksei Diuzhev <89312941+itbali@users.noreply.github.com>
2024-05-30 08:24:41 +0200

    12-add-get-expense-option-to-api (#13)

--- src/app/providers/Query.tsx
+++ src/app/providers/Query.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { PropsWithChildren } from 'react';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({ defaultOptions: { queries: { retry: 0 } } });
 
 const QueryProvider = ({ children }: PropsWithChildren) => {
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
--- src/entities/user/ui/Profile.tsx
+++ src/entities/user/ui/Profile.tsx
@@ -1,11 +1,22 @@
 import useUserStore from 'src/entities/user/model/store/useUserStore.ts';
 import selectUserData from 'src/entities/user/model/selectors/selectUserData.ts';
+import { Link } from 'react-router-dom';
+import { RoutesEnum } from 'src/shared/constants/routesEnum.ts';
 
 const Profile = () => {
   const userData = useUserStore(selectUserData);
+
   return (
     <div>
-      {userData?.email} - isVerified: {`${userData?.isVerified}`}
+      <h1>Profile Page</h1>
+      <Link to={RoutesEnum.Root}>Go Home</Link>
+      <br />
+      {userData?.email}
+      <br />
+      isVerified: {`${userData?.isVerified}`}
+      <br />
+      login:{userData?.login}
+      <br />
     </div>
   );
 };
