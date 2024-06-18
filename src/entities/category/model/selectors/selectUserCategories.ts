b6d65acb64461b928abf7fa8ab359e07fd33e0be
Nikita Tyunev <tyunev.nik89@gmail.com>
2024-06-18 22:28:11 +0300

    removed selectors related useCategoryStore

--- src/entities/category/model/selectors/selectUserCategories.ts
+++
@@ -1,5 +0,0 @@
-import { TCategoryStore } from 'src/entities/category/model/store/useCategoryStore.ts';
-
-const selectUserCategories = (state: TCategoryStore) => state.useCategories;
-
-export default selectUserCategories;
