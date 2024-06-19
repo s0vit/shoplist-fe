ba155d1b41dcbf43ac914d40021e180c5dfcf0ed
Nikita Tyunev <tyunev.nik89@gmail.com>
2024-06-19 09:23:03 +0300

    refactor createSelectors function

--- src/utils/helpers/createSelectors.ts
+++ src/utils/helpers/createSelectors.ts
@@ -7,8 +7,7 @@ const createSelectors = <S extends UseBoundStore<StoreApi<object>>>(_store: S) =
   store.use = {};
 
   for (const k of Object.keys(store.getState())) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (store.use as any)[k] = () => store((s) => s[k as keyof typeof s]);
+    (store.use as unknown as Record<string, () => never>)[k] = () => store((s) => s[k as keyof typeof s]);
   }
 
   return store;
