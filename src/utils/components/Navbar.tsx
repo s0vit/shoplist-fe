681f7de7c697ea42fb56dd7ab4be96cdf40777bf
AleksandraBoch <shemaFoxy@gmail.com>
2024-06-28 18:05:04 +0300

    menu with profile and logIn/LogOut added

--- src/utils/components/Navbar.tsx
+++ src/utils/components/Navbar.tsx
@@ -1,7 +1,20 @@
-import { AppBar, Box, Button, IconButton, Toolbar, Typography, useTheme } from '@mui/material';
+import {
+  ListItemIcon,
+  AppBar,
+  Avatar,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+  useTheme,
+  Divider,
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import useUserStore from 'src/entities/user/model/store/_useUserStore.ts';
-import { useContext, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { ColorModeContext } from 'src/app/providers/Theme.tsx';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
@@ -13,6 +26,7 @@ import useLoadCategories from 'src/entities/category/hooks/useLoadCategories.ts'
 import useLoadPaymentSources from 'src/entities/paymentSource/hooks/useLoadPaymentSources.ts';
 import useWindowWidth from 'src/utils/hooks/useWindowWidth.ts';
 import useLogout from 'src/utils/hooks/useLogout.ts';
+import { Logout } from '@mui/icons-material';
 
 const Navbar = () => {
   const isLoggedIn = useUserStore.use.user?.() !== undefined;
@@ -21,6 +35,16 @@ const Navbar = () => {
   const navigate = useNavigate();
   const theme = useTheme();
   const { isDesktopWidth } = useWindowWidth();
+
+  //все для кнопки
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   useLoadExpenses({ shouldFetchOnLoad: isVerified });
   useLoadCategories(isVerified);
@@ -54,9 +78,69 @@ const Navbar = () => {
             <Typography variant="h6" sx={{ flexGrow: 1 }}>
               Shoplist {`${import.meta.env.PACKAGE_VERSION}`}
             </Typography>
-            <Button color="inherit" onClick={handleLoginClick}>
-              {isLoggedIn ? 'Logout' : 'Login'}
-            </Button>
+
+            <>
+              <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+                <Tooltip title="Account settings">
+                  <IconButton
+                    onClick={handleClick}
+                    size="small"
+                    sx={{ ml: 2 }}
+                    aria-controls={open ? 'account-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                  >
+                    <Avatar sx={{ width: 32, height: 32 }}></Avatar>
+                  </IconButton>
+                </Tooltip>
+              </Box>
+              <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                PaperProps={{
+                  elevation: 0,
+                  sx: {
+                    overflow: 'visible',
+                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                    mt: 1.5,
+                    '& .MuiAvatar-root': {
+                      width: 32,
+                      height: 32,
+                      ml: -0.5,
+                      mr: 1,
+                    },
+                    '&::before': {
+                      content: '""',
+                      display: 'block',
+                      position: 'absolute',
+                      top: 0,
+                      right: 14,
+                      width: 10,
+                      height: 10,
+                      bgcolor: 'background.paper',
+                      transform: 'translateY(-50%) rotate(45deg)',
+                      zIndex: 0,
+                    },
+                  },
+                }}
+                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+              >
+                <MenuItem onClick={handleClose}>
+                  <Avatar /> Profile
+                </MenuItem>
+                <Divider />
+                <MenuItem onClick={handleLoginClick}>
+                  <ListItemIcon>
+                    <Logout fontSize="small" />
+                  </ListItemIcon>
+                  {isLoggedIn ? 'Logout' : 'Login'}
+                </MenuItem>
+              </Menu>
+            </>
             <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
               {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
             </IconButton>
