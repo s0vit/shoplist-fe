fa682b832442541e50000ccdeb5e9e7eccd2500c
AleksandraBoch <shemaFoxy@gmail.com>
2024-06-29 22:02:31 +0300

    refactoring

--- src/utils/components/Navbar.tsx
+++ src/utils/components/Navbar.tsx
@@ -45,7 +45,7 @@ const Navbar = () => {
             <Typography variant="h6" sx={{ flexGrow: 1 }}>
               Shoplist {`${import.meta.env.PACKAGE_VERSION}`}
             </Typography>
-            {isLoggedIn ? <NavBarMenu /> : ''}
+            {isLoggedIn && <NavBarMenu />}
 
             <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
               {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
