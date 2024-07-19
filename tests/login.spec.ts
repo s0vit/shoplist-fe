f1d9c453c85052b61d12d8b3a335c4a43dedc2fa
Diuzhev <a.dyuzhev@iteco-inno.ru>
2024-07-19 18:51:30 +0300

    added playwright and login test

---
+++ tests/login.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+
+const url = process.env.CURRENT_URL || 'http://localhost:5173/';
+
+test.describe('login', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(url);
+  });
+  test('to login correct', async ({ page }) => {
+    const loginInput = page.locator(`input[type="text"]`);
+    await loginInput.waitFor();
+
+    const passwordInput = page.locator(`input[type="password"]`);
+    await passwordInput.waitFor();
+
+    const submitButton = page.locator(`button[type="submit"]`);
+    await submitButton.waitFor();
+
+    const currentUrl = page.url();
+    await loginInput.fill(process.env.TEST_USERNAME!);
+    await passwordInput.fill(process.env.TEST_PASSWORD!);
+
+    expect(currentUrl).toBe(`${url}?to=/`);
+
+    await submitButton.click();
+    await page.waitForURL(url, { timeout: 60000 });
+    await expect(page).toHaveURL(url);
+  });
+});
