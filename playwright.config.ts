b2e54b960e6bb677f19c6ad880ca947a5cd5208a
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-07-19 20:34:03 +0200

    chore: Update Playwright configuration to use 'github' reporter in CI environment

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,14 +20,14 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'github' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
   },
   shard: { total: 1, current: 1 },
   /* Configure projects for major browsers */
