4c637aa137b06206475c495f92099a1317329591
shevchenkooleg <96664226+shevchenkooleg@users.noreply.github.com>
2024-08-25 12:42:34 +0300

    default theme from user settings (#127)
    * added able to Get default theme from user settings
    
    * The logic of saving settings has been changed. Added the ability to store settings in local storage. excluded "save" button in the settings section. Config changes saved directly at the time of the change.
    
    ---------
    
    Co-authored-by: Alex Duzhev <alexey.duzhev@tmrw.com>

--- src/entities/userSettings/model/store/useUserSettingsStore.ts
+++ src/entities/userSettings/model/store/useUserSettingsStore.ts
@@ -3,19 +3,27 @@ import { CURRENCIES, languageToCurrencyMap } from 'src/shared/constants/currenci
 import { browserLanguageToLanguageMap, LANGUAGES_ENUM } from 'src/shared/constants/Locales';
 import createSelectors from 'src/utils/helpers/createSelectors.ts';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 type TTempConfig = Omit<TConfig, '_id'> & { _id?: string };
+const localStorageData = localStorage.getItem('userConfig');
+let localStorageConfig: TTempConfig = {} as TTempConfig;
+
+if (localStorageData) {
+  localStorageConfig = JSON.parse(localStorageData).state as TTempConfig;
+}
+
 const defaultSettings: TTempConfig = {
-  currency: languageToCurrencyMap[navigator.language] || CURRENCIES.USD,
-  language: browserLanguageToLanguageMap[navigator.language] || LANGUAGES_ENUM.EN,
-  theme: THEME_ENUM.SYSTEM,
-  showCategoryColours: true,
-  showSourceColours: true,
-  showCategoryNames: true,
-  showSharedCategories: true,
-  showSharedExpenses: true,
-  showSharedSources: true,
-  showSourceNames: true,
+  currency: localStorageConfig.currency ?? (languageToCurrencyMap[navigator.language] || CURRENCIES.USD),
+  language: localStorageConfig.language ?? (browserLanguageToLanguageMap[navigator.language] || LANGUAGES_ENUM.EN),
+  theme: localStorageConfig.theme ?? THEME_ENUM.SYSTEM,
+  showCategoryColours: localStorageConfig.showCategoryColours ?? false,
+  showSourceColours: localStorageConfig.showSourceColours ?? false,
+  showCategoryNames: localStorageConfig.showCategoryNames ?? false,
+  showSharedCategories: localStorageConfig.showSharedCategories ?? false,
+  showSharedExpenses: localStorageConfig.showSharedExpenses ?? false,
+  showSharedSources: localStorageConfig.showSharedSources ?? false,
+  showSourceNames: localStorageConfig.showSourceNames ?? false,
 };
 
 type TUserConfigStore = {
@@ -24,10 +32,20 @@ type TUserConfigStore = {
   resetStore: () => void;
 };
 
-const _useUserConfigStore = create<TUserConfigStore>((set) => ({
-  config: defaultSettings,
-  setConfig: (config) => set(() => ({ config })),
-  resetStore: () => set(() => ({ config: undefined })),
-}));
+const _useUserConfigStore = create<TUserConfigStore, [['zustand/persist', TTempConfig]]>(
+  persist(
+    (set) => ({
+      config: defaultSettings,
+      setConfig: (config) => {
+        set(() => ({ config }));
+      },
+      resetStore: () => set(() => ({ config: undefined })),
+    }),
+    {
+      name: 'userConfig',
+      partialize: (state) => state.config,
+    },
+  ),
+);
 
 export default createSelectors(_useUserConfigStore);
