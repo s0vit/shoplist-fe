a102b6fa9964b973e7399dd6c0efbe758fec72c1
Oleg <shevchenko.oleg.87@gmail.com>
2024-09-02 20:17:49 +0300

    add vite-plugin-static-copy, add changes in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import version from 'vite-plugin-package-version';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), version()],
+  plugins: [
+    react(),
+    version(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: path.resolve(__dirname, 'locales/') + '/[!.]*', // 1️⃣
+          dest: './locales/', // 2️⃣
+        },
+      ],
+    }),
+  ],
   optimizeDeps: {
     include: ['@mui/material', '@emotion/react', '@emotion/styled', '@mui/x-date-pickers', 'date-fns'],
   },
