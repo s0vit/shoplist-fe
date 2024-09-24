f7a3e594c129341ca0ab2721200e1b2a0ec08afd
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-09-24 23:22:54 +0200

    spent this month is now shown on all devices

--- src/entities/expenses/ui/SpentThisMonth.tsx
+++ src/entities/expenses/ui/SpentThisMonth.tsx
@@ -5,12 +5,16 @@ import { CURRENCIES, currencies } from 'src/shared/constants/currencies';
 import calculateTotalAmount from 'src/utils/helpers/calculateTotalAmmount';
 import useUserSettingsStore from 'src/entities/userSettings/model/store/useUserSettingsStore.ts';
 import { useTranslation } from 'react-i18next';
+import useWindowWidth from 'src/utils/hooks/useWindowWidth.ts';
 
 const SpentThisMonth = () => {
   const defaultCurrency = useUserSettingsStore.use.config().currency;
   const [currency, setCurrency] = useState<CURRENCIES>(defaultCurrency ?? CURRENCIES.EUR);
   const { userExpenses } = useLoadExpenses({ shouldFetchOnLoad: true });
   const { t } = useTranslation('homePage');
+  const { windowHeight } = useWindowWidth();
+
+  const isBigScreen = windowHeight > 740;
 
   const currentMonth = new Date().getMonth();
   const currentYear = new Date().getFullYear();
@@ -22,18 +26,18 @@ const SpentThisMonth = () => {
   );
 
   return (
-    <Paper>
+    <Paper sx={{ pb: 2, mb: 1 }}>
       <Typography variant="h6" textAlign="center">
         {t('Spent this month:')}
       </Typography>
       <Stack textAlign="center" direction="row" spacing={2} justifyContent="center">
-        <Typography variant="h3" color="primary">
+        <Typography variant={isBigScreen ? 'h3' : 'h4'} color="primary">
           {calculateTotalAmount(filteredExpenses, currency)}
         </Typography>
         <FormControl>
           <Select
             autoWidth
-            sx={{ fontSize: '2rem' }}
+            sx={{ fontSize: isBigScreen ? '2rem' : '1.5rem' }}
             variant="standard"
             value={currency}
             onChange={(e) => setCurrency(e.target.value as CURRENCIES)}
