8747f4cefa6e14a4a0362d6c5528fce345adb3de
Oleg <shevchenko.oleg.87@gmail.com>
2024-10-15 21:00:44 +0300

    implemented button for hot-create paymentSources/category on corresponding pages

--- src/pages/CategoryPage/CategoryPage.tsx
+++ src/pages/CategoryPage/CategoryPage.tsx
@@ -1,16 +1,31 @@
-import { Box, Divider, Paper, Typography } from '@mui/material';
+import { Box, Divider, IconButton, Paper, Typography } from '@mui/material';
 import CategoriesGrid from 'src/entities/category/ui/CategoriesGrid.tsx';
 import useLoadCategories from 'src/entities/category/hooks/useLoadCategories.ts';
 import { useTranslation } from 'react-i18next';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import useCategoryStore from 'src/entities/category/model/store/useCategoryStore.ts';
 
 const CategoryPage = () => {
   const { userCategories, isCategoriesLoading, fetchCategories } = useLoadCategories();
   const { t } = useTranslation('categories');
+  const setIsCategoryModalOpen = useCategoryStore.use.setIsCategoryModalOpen();
 
   return (
     <Paper>
-      <Box padding={2}>
+      <Box padding={2} style={{ position: 'relative' }}>
         <Typography variant="h3">{t('Categories')}</Typography>
+        <IconButton
+          size="small"
+          disabled={false}
+          color="primary"
+          onClick={() => {
+            setIsCategoryModalOpen(true);
+          }}
+          style={{ position: 'absolute', right: '20px', top: '26px' }}
+        >
+          <AddCircleOutlineIcon fontSize="large" />
+        </IconButton>
+
         <Divider />
         <br />
         {isCategoriesLoading && <Typography>{t('Loading...')}</Typography>}
--- src/pages/PaymentSourcesPage/PaymentSourcesPage.tsx
+++ src/pages/PaymentSourcesPage/PaymentSourcesPage.tsx
@@ -1,16 +1,30 @@
 import useLoadPaymentSources from 'src/entities/paymentSource/hooks/useLoadPaymentSources.ts';
-import { Box, Divider, Paper, Typography } from '@mui/material';
+import { Box, Divider, IconButton, Paper, Typography } from '@mui/material';
 import PaymentSourcesGrid from 'src/entities/paymentSource/ui/PaymentSourcesGrid.tsx';
 import { useTranslation } from 'react-i18next';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import usePaymentSourcesStore from 'src/entities/paymentSource/model/store/usePaymentSourcesStore.ts';
 
 const PaymentSourcesPage = () => {
   const { userPaymentSources, isPaymentSourcesLoading, fetchPaymentSources } = useLoadPaymentSources();
   const { t } = useTranslation('accounts');
+  const setIsPaymentSourceModalOpen = usePaymentSourcesStore.use.setIsPaymentSourceModalOpen();
 
   return (
     <Paper>
-      <Box padding={2}>
+      <Box padding={2} style={{ position: 'relative' }}>
         <Typography variant="h3">{t('Accounts')}</Typography>
+        <IconButton
+          size="small"
+          disabled={false}
+          color="primary"
+          onClick={() => {
+            setIsPaymentSourceModalOpen(true);
+          }}
+          style={{ position: 'absolute', right: '20px', top: '26px' }}
+        >
+          <AddCircleOutlineIcon fontSize="large" />
+        </IconButton>
         <Divider />
         <br />
         {isPaymentSourcesLoading && <Typography>Loading...</Typography>}
