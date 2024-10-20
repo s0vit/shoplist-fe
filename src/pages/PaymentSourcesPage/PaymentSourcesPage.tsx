372a186f1f6bfcb7fea14e5e98911525f4b0a744
shevchenkooleg <96664226+shevchenkooleg@users.noreply.github.com>
2024-10-20 11:16:08 +0300

    #134 source/catecory delete modal window (#147)
    * add modal window before delete category/payment
    
    * fix lint error
    
    * fix lint error, fix some review comment
    
    * fix review comments
    
    * fix review comments

--- src/pages/PaymentSourcesPage/PaymentSourcesPage.tsx
+++ src/pages/PaymentSourcesPage/PaymentSourcesPage.tsx
@@ -2,13 +2,35 @@ import useLoadPaymentSources from 'src/entities/paymentSource/hooks/useLoadPayme
 import { Box, Divider, IconButton, Paper, Typography } from '@mui/material';
 import PaymentSourcesGrid from 'src/entities/paymentSource/ui/PaymentSourcesGrid.tsx';
 import { useTranslation } from 'react-i18next';
+import DeleteCategoryDialog from 'src/widgets/Modal/DeleteCategoryDialog.tsx';
+import { useState } from 'react';
+import { deletePaymentSource, TPaymentSource } from 'src/shared/api/paymentsSourceApi.ts';
+import { useMutation } from '@tanstack/react-query';
+import handleError from 'src/utils/errorHandler';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import usePaymentSourcesStore from 'src/entities/paymentSource/model/store/usePaymentSourcesStore.ts';
 
 const PaymentSourcesPage = () => {
   const { userPaymentSources, isPaymentSourcesLoading, fetchPaymentSources } = useLoadPaymentSources();
+  const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+  const [deletingPaymentSource, setDeletingPaymentSource] = useState<TPaymentSource>();
   const { t } = useTranslation('accounts');
   const setIsPaymentSourceModalOpen = usePaymentSourcesStore.use.setIsPaymentSourceModalOpen();
+
+  const handleOpenDeleteDialog = (paymentSource: TPaymentSource) => {
+    setOpenDeleteDialog(true);
+    setDeletingPaymentSource(paymentSource);
+  };
+
+  const { mutate: requestDeletePaymentSource, isPending: isUpdating } = useMutation({
+    mutationFn: deletePaymentSource,
+    onError: (error) => handleError(error),
+    onSuccess: async () => {
+      await fetchPaymentSources();
+      setOpenDeleteDialog(false);
+      setDeletingPaymentSource(undefined);
+    },
+  });
 
   return (
     <Paper>
@@ -29,8 +51,15 @@ const PaymentSourcesPage = () => {
         <br />
         {isPaymentSourcesLoading && <Typography>Loading...</Typography>}
         {userPaymentSources && (
-          <PaymentSourcesGrid paymentSources={userPaymentSources} refetchPaymentSources={fetchPaymentSources} />
+          <PaymentSourcesGrid paymentSources={userPaymentSources} handleOpenDeleteDialog={handleOpenDeleteDialog} />
         )}
+        <DeleteCategoryDialog
+          openDeleteDialog={openDeleteDialog}
+          setOpenDeleteDialog={setOpenDeleteDialog}
+          handleConfirmDelete={requestDeletePaymentSource}
+          item={deletingPaymentSource}
+          isUpdating={isUpdating}
+        />
       </Box>
     </Paper>
   );
