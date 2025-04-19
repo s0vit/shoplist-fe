aaa10406e233743cf7c0437781c90b47c49dca3b
passrise <ich.nemoroz@gmail.com>
2025-04-19 15:16:33 +0200

    fix: errors in console

--- src/stories/Buttons.stories.tsx
+++ src/stories/Buttons.stories.tsx
@@ -7,12 +7,12 @@ const meta: Meta<typeof PrimaryButton> = {
   tags: ['autodocs'],
   argTypes: {
     label: { control: 'text' },
-    textColor: { control: 'color' },
-    backgroundColor: { control: 'color' },
-    borderColor: { control: 'text' },
+    $textColor: { control: 'color' },
+    $backgroundColor: { control: 'color' },
+    $borderColor: { control: 'text' },
     width: { control: 'text' },
     height: { control: 'text' },
-    variant: {
+    $variant: {
       control: { type: 'radio' },
       options: ['contained', 'outlined'],
     },
@@ -25,105 +25,105 @@ type TStory = StoryObj<typeof PrimaryButton>;
 export const SaveDark: TStory = {
   args: {
     label: 'Сохранить',
-    variant: 'contained',
-    textColor: '#FFFFFF',
-    backgroundColor: '#000000',
-    borderColor: '#000000',
+    $variant: 'contained',
+    $textColor: '#FFFFFF',
+    $backgroundColor: '#000000',
+    $borderColor: '#000000',
   },
 };
 
 export const SaveLight: TStory = {
   args: {
     label: 'Сохранить',
-    variant: 'contained',
-    textColor: '#000000',
-    backgroundColor: '#FFFFFF',
-    borderColor: '#000000',
+    $variant: 'contained',
+    $textColor: '#000000',
+    $backgroundColor: '#FFFFFF',
+    $borderColor: '#000000',
   },
 };
 
 export const ClearDark: TStory = {
   args: {
     label: 'Очистить',
-    textColor: '#FFFFFF',
-    variant: 'contained',
-    backgroundColor: '#151516',
-    borderColor: '#FFFFFF',
+    $textColor: '#FFFFFF',
+    $variant: 'contained',
+    $backgroundColor: '#151516',
+    $borderColor: '#FFFFFF',
   },
 };
 
 export const ClearLight: TStory = {
   args: {
     label: 'Очистить',
-    textColor: '#000000',
-    variant: 'outlined',
-    borderColor: '#000000',
+    $textColor: '#000000',
+    $variant: 'outlined',
+    $borderColor: '#000000',
   },
 };
 
 export const CancelDark: TStory = {
   args: {
     label: 'Отменить',
-    variant: 'contained',
-    textColor: '#FFFFFF',
-    backgroundColor: '#151516',
-    borderColor: '#FFFFFF',
+    $variant: 'contained',
+    $textColor: '#FFFFFF',
+    $backgroundColor: '#151516',
+    $borderColor: '#FFFFFF',
   },
 };
 
 export const CancelLight: TStory = {
   args: {
     label: 'Отменить',
-    variant: 'outlined',
-    textColor: '#000000',
-    borderColor: '#000000',
+    $variant: 'outlined',
+    $textColor: '#000000',
+    $borderColor: '#000000',
   },
 };
 
 export const RegisterDark: TStory = {
   args: {
     label: 'Зарегистрироваться',
-    variant: 'contained',
+    $variant: 'contained',
     width: '296px',
     height: '53px',
-    textColor: '#FFFFFF',
-    backgroundColor: '#000000',
-    borderColor: '#000000',
+    $textColor: '#FFFFFF',
+    $backgroundColor: '#000000',
+    $borderColor: '#000000',
   },
 };
 
 export const RegisterLight: TStory = {
   args: {
     label: 'Зарегистрироваться',
-    variant: 'contained',
+    $variant: 'contained',
     width: '296px',
     height: '53px',
-    textColor: '#000000',
-    backgroundColor: '#FFFFFF',
-    borderColor: '#000000',
+    $textColor: '#000000',
+    $backgroundColor: '#FFFFFF',
+    $borderColor: '#000000',
   },
 };
 
 export const LoginDark: TStory = {
   args: {
     label: 'Войти',
-    variant: 'contained',
+    $variant: 'contained',
     width: '296px',
     height: '53px',
-    textColor: '#FFFFFF',
-    backgroundColor: '#000000',
-    borderColor: '#000000',
+    $textColor: '#FFFFFF',
+    $backgroundColor: '#000000',
+    $borderColor: '#000000',
   },
 };
 
 export const LoginLight: TStory = {
   args: {
     label: 'Войти',
-    variant: 'contained',
+    $variant: 'contained',
     width: '296px',
     height: '53px',
-    textColor: '#000000',
-    backgroundColor: '#FFFFFF',
-    borderColor: '#000000',
+    $textColor: '#000000',
+    $backgroundColor: '#FFFFFF',
+    $borderColor: '#000000',
   },
 };
