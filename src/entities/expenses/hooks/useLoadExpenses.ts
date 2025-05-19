113d7ef534421d69b1253484feff51af4c5e6b87
passrise <ich.nemoroz@gmail.com>
2025-05-19 17:29:19 +0200

    fix: reset filters on add expense

--- src/entities/expenses/hooks/useLoadExpenses.ts
+++ src/entities/expenses/hooks/useLoadExpenses.ts
@@ -1,39 +1,48 @@
 import { useQuery } from '@tanstack/react-query';
-import { isAxiosError } from 'axios';
 import { useEffect } from 'react';
 import useExpensesStore from 'src/entities/expenses/model/store/useExpensesStore.ts';
 import { getExpenses, TGetExpenseQuery, TGetExpensesResponse } from 'src/shared/api/expenseApi.ts';
 import { TErrorResponse } from 'src/shared/api/rootApi.ts';
 import handleError from 'src/utils/errorHandler.ts';
 import useStableCallback from 'src/utils/hooks/useStableCallback.ts';
+import useFiltersStoreForExpenses from 'src/entities/filters/models/store/FiltersStore.ts';
+import useDebouncedValue from 'src/utils/hooks/useDebouncedValue.ts';
+import _useUserStore from 'src/entities/user/model/store/useUserStore.ts';
+import { TFilterForQueryTypes } from 'src/entities/filters/models/types/types.ts';
 
 type TUseLoadExpensesArgs =
   | {
-      query?: TGetExpenseQuery;
-      withShared?: boolean;
       onFetchFinish?: () => void;
       shouldFetchOnLoad?: boolean;
+      withShared?: boolean;
     }
   | undefined;
 
-const useLoadExpenses = ({
-  shouldFetchOnLoad = false,
-  query,
-  onFetchFinish,
-  withShared,
-}: TUseLoadExpensesArgs = {}) => {
+const formatFiltersToQuery = (filters: TFilterForQueryTypes): TGetExpenseQuery => ({
+  ...filters,
+  createdStartDate: filters.createdStartDate ? new Date(filters.createdStartDate) : undefined,
+  createdEndDate: filters.createdEndDate ? new Date(filters.createdEndDate) : undefined,
+  amountStart: filters.amountStart ? parseFloat(filters.amountStart) : undefined,
+  amountEnd: filters.amountEnd ? parseFloat(filters.amountEnd) : undefined,
+  skip: filters.skip ? parseInt(filters.skip, 10) : undefined,
+  limit: filters.limit ? parseInt(filters.limit, 10) : undefined,
+});
+
+const useLoadExpenses = ({ shouldFetchOnLoad = false, onFetchFinish, withShared }: TUseLoadExpensesArgs = {}) => {
   const setUserExpenses = useExpensesStore.use.setUserExpenses();
   const userExpenses = useExpensesStore.use.userExpenses();
+  const filters = useFiltersStoreForExpenses.use.filter();
+  const isVerified = _useUserStore.use.user?.()?.isVerified;
+  const debouncedQuery = useDebouncedValue(filters, 300);
 
   const {
     data: expenses,
     isPending: isExpensesLoading,
     error: expensesError,
-    refetch,
   } = useQuery<TGetExpensesResponse, TErrorResponse>({
-    queryKey: ['expenses'],
-    queryFn: () => getExpenses(query),
-    enabled: shouldFetchOnLoad,
+    queryKey: ['expenses', debouncedQuery],
+    queryFn: () => getExpenses(formatFiltersToQuery(debouncedQuery)),
+    enabled: shouldFetchOnLoad && isVerified,
   });
 
   if (withShared) {
@@ -47,25 +56,22 @@ const useLoadExpenses = ({
   }, [expensesError, shouldFetchOnLoad]);
 
   useEffect(() => {
-    if (!isExpensesLoading) {
-      setUserExpenses(expenses || []);
-      if (onFetchFinish) onFetchFinish();
+    if (!isExpensesLoading && expenses) {
+      setUserExpenses(expenses);
+      onFetchFinish?.();
     }
   }, [expenses, isExpensesLoading, setUserExpenses, onFetchFinish]);
 
-  const fetchExpenses = useStableCallback(async (newQuery?: TGetExpenseQuery) => {
-    if (newQuery) {
-      query = newQuery;
+  const fetchExpenses = useStableCallback(async () => {
+    const formattedQuery = formatFiltersToQuery(filters);
+
+    try {
+      const result = await getExpenses(formattedQuery);
+      setUserExpenses(result);
+      onFetchFinish?.();
+    } catch (err) {
+      handleError(err);
     }
-
-    const newData = await refetch();
-
-    if (isAxiosError(newData)) {
-      handleError(newData);
-    }
-
-    setUserExpenses((newData.data as unknown as TGetExpensesResponse) || []);
-    if (onFetchFinish) onFetchFinish();
   });
 
   return {
