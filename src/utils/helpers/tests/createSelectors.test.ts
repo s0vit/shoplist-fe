36a310c5091a06ada8cb57884e90e81fda5ebe66
Aida Vafina <vai2706@bk.ru>
2025-05-24 18:11:35 +0300

    добавила юнит тесты

---
+++ src/utils/helpers/tests/createSelectors.test.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect } from 'vitest';
+import { create } from 'zustand';
+import createSelectors from './../createSelectors';
+
+describe('createSelectors', () => {
+  const useStoreBase = create(() => ({
+    count: 42,
+    user: { name: 'котёнок', age: 3 },
+  }));
+
+  const store = createSelectors(useStoreBase);
+
+  it('должен иметь поле use', () => {
+    expect(store.use).toBeDefined();
+  });
+});
