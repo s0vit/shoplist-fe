4cf13b8a96b4245660ef1c31e1a0fd360afce69e
Lyubov Kabanova <lyubovrausch@gmail.com>
2025-07-25 20:23:17 +0300

    fix(fix): fixed adding expenses

--- src/entities/expenses/ui/ExpensesTable.tsx
+++ src/entities/expenses/ui/ExpensesTable.tsx
@@ -8,7 +8,6 @@ import _useUserStore from 'src/entities/user/model/store/useUserStore.ts';
 import useInfiniteExpenses from 'src/entities/expenses/hooks/useInfiniteExpenses.ts';
 import SkeletonGroup from 'src/utils/components/Skeleton.tsx';
 import useLoadExpenses from 'src/entities/expenses/hooks/useLoadExpenses.ts';
-// import useExpensesStore from 'src/entities/expenses/model/store/useExpensesStore.ts';
 import useFiltersStoreForExpenses from 'src/entities/filters/models/store/FiltersStore.ts';
 
 const groupExpensesByDate = (expenses: TExpense[]) => {
@@ -29,31 +28,24 @@ const ExpensesTable = () => {
   const queryClient = useQueryClient();
   const filters = useFiltersStoreForExpenses.use.filter();
 
-  // 1. Загружаем текущий месяц через useLoadExpenses
   const { userExpenses: currentMonthExpenses, isExpensesLoading } = useLoadExpenses({
     shouldFetchOnLoad: isVerified,
     filters: {
-      // Теперь filters будет принят
       ...filters,
       createdStartDate: format(new Date(), 'yyyy-MM-01'),
       createdEndDate: format(new Date(), 'yyyy-MM-dd'),
     },
   });
 
-  // 2. Загружаем предыдущие месяцы через useInfiniteExpenses
   const { data: infiniteData, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteExpenses();
 
-  // 3. Объединяем данные
   const allExpenses = [...(currentMonthExpenses || []), ...(infiniteData?.pages.flat() || [])];
 
   const groupedExpenses = groupExpensesByDate(allExpenses);
 
-  // Обработка удаления
   const { mutate: handleDeleteExpense } = useMutation({
     mutationFn: deleteExpense,
     onSuccess: () => {
-      // Инвалидируем оба запроса
-
       queryClient.invalidateQueries({ queryKey: ['expenses'] });
       queryClient.invalidateQueries({ queryKey: ['expenses-infinite'] });
     },
