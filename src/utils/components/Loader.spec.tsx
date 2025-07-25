1d6f0a2e2e47b3f4a18f8df6da359af0ac4dfef2
Ruslan M <158747116+marozny@users.noreply.github.com>
2025-07-25 08:44:20 +0200

    Make tests for Loader component (#216)
    * test(Loader): add new unit tests
    
    * test(Loader): rewrite snapshot test with asFragment

---
+++ src/utils/components/Loader.spec.tsx
@@ -0,0 +1,49 @@
+import { render, screen, cleanup } from '@testing-library/react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Loader from 'src/utils/components/Loader.tsx';
+import { describe, expect, it, afterEach, beforeEach } from 'vitest';
+
+let renderResult: ReturnType<typeof render>;
+
+const renderWithTheme = () => {
+  const theme = createTheme();
+
+  return render(
+    <ThemeProvider theme={theme}>
+      <Loader />
+    </ThemeProvider>,
+  );
+};
+
+describe('Loader', () => {
+  beforeEach(() => {
+    renderResult = renderWithTheme();
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('render loading img correctly', () => {
+    const img = screen.getByAltText(/loading/i);
+    expect(img).not.toBeNull();
+    expect(img.tagName).toBe('IMG');
+  });
+
+  it('container with all styles exists', () => {
+    const img = screen.getByAltText(/loading/i);
+    const box = img.closest('div');
+    expect(box).not.toBeNull();
+
+    const styles = window.getComputedStyle(box!);
+    expect(styles.width).toMatch(/\d+px/);
+    expect(styles.height).toMatch(/\d+px/);
+    expect(styles.position).toBe('fixed');
+    expect(styles.zIndex).toBe('9999');
+    expect(styles.backdropFilter).toBe('blur(5px)');
+  });
+
+  it('matches snapshot', () => {
+    expect(renderResult.asFragment()).toMatchSnapshot();
+  });
+});
